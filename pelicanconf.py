0ad6b1686a93eb0248fd886c705c3a0bf9682621
binkocd <{ID}+{username}@users.noreply.github.com>
2019-03-19 21:08:17 -0700

    404 and such

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,10 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL=(('LinkedIn', 'https://www.linkedin.com/in/joshuarobertbailey'),
+('Github', 'https://github.com/binkocd'),
+('Bitbucket', 'https://bitbucket.org/binkocd'),
+('Gitlab', 'https://gitlab.com/binkocd'))
 
 DEFAULT_PAGINATION = 10
 
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://jrbit.me'
+SITEURL = 'http://binkocd.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
