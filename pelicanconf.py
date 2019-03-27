2c4cbf7ed28fad6d695e60dbaff8848eeb58c2aa
binkocd <{ID}+{username}@users.noreply.github.com>
2019-03-26 18:07:25 -0700

    theme and site tweaks

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,18 +20,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('LinkedIn', 'https://www.linkedin.com/in/joshuarobertbailey'),
+         ('Github', 'https://github.com/binkocd'),
+         ('Bitbucket', 'https://bitbucket.org/binkocd'),
+         ('Gitlab', 'https://gitlab.com/binkocd'))
+
 
 # Social widget
-SOCIAL=(('LinkedIn', 'https://www.linkedin.com/in/joshuarobertbailey'),
-('Github', 'https://github.com/binkocd'),
-('Bitbucket', 'https://bitbucket.org/binkocd'),
-('Gitlab', 'https://gitlab.com/binkocd'))
+SOCIAL= (('LinkedIn', 'https://www.linkedin.com/in/joshuarobertbailey'),
+         ('Github', 'https://github.com/binkocd'),
+         ('Bitbucket', 'https://bitbucket.org/binkocd'),
+         ('Gitlab', 'https://gitlab.com/binkocd'))
 
 DEFAULT_PAGINATION = 10
-
+THEME = 'Flex'
+#SITELOGO = SITEURL + '/images/profile.png'
+MAIN_MENU = True
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
